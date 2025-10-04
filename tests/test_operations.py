aba5e92e3e23083200e2d922e156a45f35f079c3
sultan shah <sultanshah101004@gmail.com>
2025-10-04 06:56:45 +0500

    Update test_operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -294,19 +294,4 @@ def test_cube() :
 
 # Additional tests can be added for other functions as needed
 
-def prime_factors(n):
-    factors = []
-    # Handle 2 separately
-    while n % 2 == 0:
-        factors.append(2)
-        n //= 2
-    # Handle odd factors
-    i = 3
-    while i * i <= n:
-        while n % i == 0:
-            factors.append(i)
-            n //= i
-        i += 2
-    if n > 2:
-        factors.append(n)
-    return factors
+
